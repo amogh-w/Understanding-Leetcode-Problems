d7c5ab772be895fe29fa52dead583bb1dedfbf1d
Amogh Warkhandkar <amoghw2025@gmail.com>
2021-06-20 19:42:12 +0530

    Added Tailwind CSS

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Footer from "./footer"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <div>
+      <Layout>Home</Layout>
+    </div>
+  )
 }
+
+export default Home
